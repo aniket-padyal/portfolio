3a1b5abf50b9559f8f4b4deec1a2dbd564c6e023
aniket-padyal <padyal10aniket@gmail.com>
2026-06-02 21:00:16 +0530

    add interactive menu

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <nav className="flex justify-between items-center p-8 bg-red-50 ">
-      <div className="text-4xl   ">aniket.dev</div>
-      <div>
-        <ul className="flex gap-4">
-          <li>hello</li>
-          <li>sdfsd</li>
-          <li>afda</li>
-        </ul>
+    <nav className="flex justify-between items-center px-6 py-3 relative w-full  ">
+      <div className="text-3xl ">random</div>
+
+      <div
+        // className="border"
+        onClick={() => {
+          setIsMenuOpen((prev) => !prev);
+        }}
+      >
+        {isMenuOpen ? <X /> : <Menu />}
+
+        {isMenuOpen ? (
+          <ul className="flex flex-col gap-1 text-right absolute right-6 mt-3  ">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        ) : null}
       </div>
     </nav>
   );
