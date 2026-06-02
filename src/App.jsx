a3f7132ad4baa4ea6286d79b29c0aadf647724d6
aniket-padyal <padyal10aniket@gmail.com>
2026-06-02 13:14:54 +0530

    implement navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/common/Navbar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center p-8 bg-red-50 ">
+      <div className="text-4xl   ">aniket.dev</div>
+      <div>
+        <ul className="flex gap-4">
+          <li>hello</li>
+          <li>sdfsd</li>
+          <li>afda</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
