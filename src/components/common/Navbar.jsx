96ffecd1f9686e82fc694c5496b251ae424aeeb5
aniket-padyal <padyal10aniket@gmail.com>
2026-06-05 21:18:20 +0530

    style preloader

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -11,15 +11,15 @@ const Navbar = () => {
   const toggleTheme = () => setIsLight((prev) => !prev);
 
   return (
-    <nav className=" relative flex items-center justify-between w-full sm:px-10 px-5 py-5  ">
-      <h1 className="text-4xl ">aniket.dev</h1>
+    <nav className="relative flex items-center justify-between w-full sm:px-10 px-5 py-5 ">
+      <h1 className="text-4xl ">random.dev</h1>
 
       {/* Menu Toggle Button */}
       <button
         type="button"
         onClick={toggleMenu}
         aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-        className="relative z-20 h-6 w-6 flex items-center justify-center cursor-pointer  sm:bg-[#ebeae8] p-6 rounded-full "
+        className="relative z-20 h-6 w-6 flex items-center justify-center cursor-pointer  sm:bg-[#ebeae8] sm:p-6 rounded-full "
       >
         <Menu
           size={25}
--- src/components/common/Preloader.jsx
+++ src/components/common/Preloader.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Preloader = () => {
   return (
-    <div className="h-dvh flex justify-center items-center ">
-      <div className="preloader-text text-center text-lg sm:text-2xl p-2 border ">
+    <div className="preloader h-dvh flex justify-center items-center ">
+      <div className="preloader-text text-center text-lg sm:text-2xl p-2  ">
         "Do not misunderstand. This is not the power of your creation!""
       </div>
     </div>
